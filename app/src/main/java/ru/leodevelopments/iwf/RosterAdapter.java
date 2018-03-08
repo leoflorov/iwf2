dd89a4b7fbdc80784b004c48695f587047ed8fd2
leoflorov <leoflorov@mail.ru>
2018-03-08 22:10:26 +0300

    Update

--- app/src/main/java/ru/leodevelopments/iwf/RosterAdapter.java
+++ app/src/main/java/ru/leodevelopments/iwf/RosterAdapter.java
@@ -1,22 +1,22 @@
 package ru.leodevelopments.iwf;
 
-        import android.content.Context;
-        import android.support.v7.widget.PopupMenu;
-        import android.support.v7.widget.RecyclerView;
-        import android.view.LayoutInflater;
-        import android.view.MenuInflater;
-        import android.view.MenuItem;
-        import android.view.View;
-        import android.view.ViewGroup;
-        import android.widget.ImageView;
-        import android.widget.TextView;
-        import android.widget.Toast;
+import android.content.Context;
+import android.support.v7.widget.PopupMenu;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-        import com.bumptech.glide.Glide;
+import com.bumptech.glide.Glide;
 
-        import java.util.List;
+import java.util.List;
 
-        import leodevelopments.iwf.R;
+import leodevelopments.iwf.R;
 
 class RosterAdapter extends RecyclerView.Adapter<RosterAdapter.MyViewHolder> {
 
